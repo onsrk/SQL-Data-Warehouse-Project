bff6ff4b463257642e92cbeadec264e7c6d2a365
onsrk <noussarekik20@gmail.com>
2026-03-31 22:28:41 +0100

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -46,13 +46,13 @@ GO
 CREATE TABLE bronze.crm_sales_details (
     sls_ord_num  NVARCHAR(50),
     sls_prd_key  NVARCHAR(50),
-    sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
-    sls_quantity INT,
-    sls_price    INT
+    sls_cust_id  NVARCHAR(50),
+    sls_order_dt NVARCHAR(50),
+    sls_ship_dt  NVARCHAR(50),
+    sls_due_dt   NVARCHAR(50),
+    sls_sales    NVARCHAR(50),
+    sls_quantity NVARCHAR(50),
+    sls_price    NVARCHAR(50)
 );
 GO
 
